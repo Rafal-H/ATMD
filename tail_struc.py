3d8fb76938eeedd164558b2518fde614c81bb575
bracketBoi <jackburns@slink-tech.co.uk>
2023-03-14 13:10:13 +0000

    added constraint on max tail struc diam

--- tail_struc.py
+++ tail_struc.py
@@ -21,7 +21,7 @@ def tail_struc_weight(tailLength, assDiam, worstForce):
     SF = 1.5
     stressYield = 345e6
     stressShear = 283e6
-    allowStress = stressYield/SF 
+    allowStress = stressYield/SF
     allowShear = stressShear/SF
     dens = 2780 #kg per cubic metre 
     E = 73.1e9 
