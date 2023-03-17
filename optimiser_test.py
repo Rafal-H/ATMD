ffb80940c7640832753996997abee65cdeb059ff
bracketBoi <jackburns@slink-tech.co.uk>
2023-03-17 14:45:37 +0000

    fix typo

--- optimiser_test.py
+++ optimiser_test.py
@@ -3,7 +3,7 @@ from scipy.optimize import minimize
 from model import full_model 
 
 x0 = np.array([16.7, 4, 8, 11])
-res = minimize(full_model, x0, callback=callbackF, method='Nelder-Mead')
+res = minimize(full_model, x0, method='Nelder-Mead')
 
 print(res)
 print(res.x)
