15d4f958c452e4d74c971308d9f222d4caca0d23
bracketBoi <jackburns@slink-tech.co.uk>
2023-03-17 14:45:08 +0000

    added optimiser test

---
+++ optimiser_test.py
@@ -0,0 +1,10 @@
+import numpy as np
+from scipy.optimize import minimize 
+from model import full_model 
+
+x0 = np.array([16.7, 4, 8, 11])
+res = minimize(full_model, x0, callback=callbackF, method='Nelder-Mead')
+
+print(res)
+print(res.x)
+print(res.message)
