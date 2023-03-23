dbccd9190f671da025127feffa6a955c01912e55
bracketBoi <jackburns@slink-tech.co.uk>
2023-03-23 14:35:52 +0000

    more opt pissery

--- optimiser_test.py
+++ optimiser_test.py
@@ -4,7 +4,8 @@ from scipy.optimize import dual_annealing
 from model import full_model 
 
 x0 = np.array([16.7, 4, 8, 11])
-res = minimize(full_model, x0, method='Nelder-Mead')
+bnds = ((10, 30), (2.4, 7), (0.1,15), (0, 45))
+res = minimize(full_model, x0, method='TNC', bounds=bnds)
 #res = minimize(full_model, x0, method='dogleg')
 #res = dual_annealing(full_model, x0)
 
