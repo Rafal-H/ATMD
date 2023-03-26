6add15f35a74c05c488fb8d5d5de18b4e10f2366
bracketBoi <jackburns@slink-tech.co.uk>
2023-03-26 12:34:21 +0100

    i do not understand

--- optimiser_test.py
+++ optimiser_test.py
@@ -3,9 +3,12 @@ from scipy.optimize import minimize
 from scipy.optimize import dual_annealing
 from model import full_model 
 
-x0 = np.array([16.7, 4, 8, 11])
-bnds = ((10, 30), (2.4, 7), (0.1,15), (0, 45))
-res = minimize(full_model, x0, method='TNC', bounds=bnds)
+#cyLength, cyDiam, tailLength, boatAng
+x0 = np.array([16.7, 9, 8, 11])
+bnds = ((10, 30), (2.4, 10), (0.1,15), (0, 45))
+optns={'maxiter':10, 'maxfev':10} 
+res = minimize(full_model, x0, method='Nelder-Mead', bounds=bnds, options=optns) 
+print("\ndone\n") 
 #res = minimize(full_model, x0, method='dogleg')
 #res = dual_annealing(full_model, x0)
 
