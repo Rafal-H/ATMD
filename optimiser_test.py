051004693fe6d0747942030afa325244b1041ac6
bracketBoi <jackburns@slink-tech.co.uk>
2023-03-26 13:01:18 +0100

    eh

--- optimiser_test.py
+++ optimiser_test.py
@@ -6,8 +6,13 @@ from model import full_model
 #cyLength, cyDiam, tailLength, boatAng
 x0 = np.array([16.7, 9, 8, 11])
 bnds = ((10, 30), (2.4, 10), (0.1,15), (0, 45))
-optns={'maxiter':10, 'maxfev':10} 
-res = minimize(full_model, x0, method='Nelder-Mead', bounds=bnds, options=optns) 
+
+res = minimize(full_model, 
+               x0, 
+               method='Nelder-Mead', 
+               bounds=bnds, 
+               options={"maxiter":10, "maxfev":10, "disp": True, "xatol": 10, "fatol": 1000} ) 
+
 print("\ndone\n") 
 #res = minimize(full_model, x0, method='dogleg')
 #res = dual_annealing(full_model, x0)
